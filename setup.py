bb5b8f1c241339c56a3a53d545b9b46b0c2eb55c
Cody Owens <cody.owens@ge.com>
2020-01-09 21:30:28 -0600

    Add some setup / version files

--- setup.py
+++ setup.py
@@ -3,22 +3,25 @@ from setuptools import setup, find_packages
 def readme():
     with open('README.md') as f:
         return f.read()
+    
+def requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 
-setup(name = 'emspy',
-      version = '0.2.4',
+setup(name = 'emsapi',
+      version = '0.0.0',
       description = 'A Python EMS RESTful API Client/Wrapper',
       long_description=readme(),
       long_description_content_type="text/markdown",
       classifiers = [
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License'
         ],
-      url = 'https://github.com/ge-flight-analytics/emspy',
+      url = 'https://github.com/ge-flight-analytics/ems-api-python',
       author = 'GE Flight Analytics',
       author_email = 'AviationAdiSupport@ge.com',
       license = 'MIT',
       packages = find_packages(exclude=("docs","tests")),
-      install_requires = ['numpy', 'pandas', 'future', 'networkx'],
+      install_requires = requirements(),
       zip_safe = False)
       
