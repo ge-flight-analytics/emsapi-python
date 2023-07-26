6132a23db994e576c84bc08d042e5b1409336dfe
Cody Owens <cody.owens@ge.com>
2023-07-26 11:07:41 -0500

    Bump version: 1.1.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def requirements():
         return f.read().splitlines()
 
 setup(name = 'emsapi',
-      version = '1.1.0',
+      version = '2.0.0',
       description = 'A Python EMS RESTful API Client/Wrapper',
       long_description=readme(),
       long_description_content_type="text/markdown",
